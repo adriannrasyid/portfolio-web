bb6b034cef0e665ad96625d7cdf776ad68cafae1
adriannrasyid <adrian.rasyid3021@gmail.com>
2025-02-21 20:12:05 +0700

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+// navbar
+window.onscroll = function () {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if(window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed');
+    }
+};
+
+
+// hambuger button
+const hamburger = document.querySelector('#hamburger');
+const navbarMenu = document.querySelector('#navbar-menu');
+
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('hamburger-active');
+    navbarMenu.classList.toggle('hidden');
+});
